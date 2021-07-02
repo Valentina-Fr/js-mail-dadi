29a7c14ab5fa73b31a67e7f29236e57adc8b2866
Valentina-Fr <v_frasca@virgilio.it>
2021-07-02 16:15:45 +0200

    Declare and assign var

---
+++ mail/js/script.js
@@ -0,0 +1,16 @@
+/* 
+1 - Creare lista indirizzi email
+2 - Chiedere all’utente la sua email
+3 - Controllare che l'email dell'utente sia nella lista
+4 - Stampare messaggio 
+*/
+
+//Dichiarazione e assegnazioni variabili
+var addresses = ["mario@gmail.com", "laura@gmail.com", "stefano@gmail.com", "maria@gmail.com"];
+var userAddress = prompt("Inserisci il tuo indirizzo e-mail");
+var welcomePlaceholder = document.getElementById("welcome");
+
+console.table(addresses);
+console.log(userAddress);
+
+
